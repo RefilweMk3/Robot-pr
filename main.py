be2fb7b75d01fdb1fc8c804ab093bf3c8260b4bd
RefilweMk3 <refilwekgwadi13@gmail.com>
2024-09-20 20:49:48 +0200

    Robot introduction

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ class robot:
         self.model = model
         self.purpose = purpose
     def intro(self):
-        return f"Hello, I am {self.name}, a {self.model} robot designed for {self.purpose}."
+        return f"Hello, I am {self.name}, A {self.model} robot designed for {self.purpose}."
 
 class SR(robot):
     def __init__(self, name, model, purpose, service_area):
         super().__init__(name, model, purpose)
         self.service_area = service_area
     def intro(self):
-        return f"Hello there I'm{self.name}, a {self.model} robot. I work in {self.service_area} for {self.purpose}."
+        return f"Hello there I'm {self.name}, An {self.model} robot. I work in {self.service_area} for {self.purpose}."
 class IR(robot):
     def __init__(self, name, model, purpose, production_line):
         super().__init__(name, model, purpose)
         self.production_line = production_line
     def intro(self):
-        return f"Greetings, I am {self.name}, a {self.model} robot, and I operate on the {self.production_line} production line for {self.purpose}."
+        return f"Greetings, I am {self.name}, An {self.model} robot, and I operate on the {self.production_line} production line for {self.purpose}."
 
 robot1 = robot("Robo1", "Basic", "general assistance")
 robot2 = SR("ServBot", "SR-200", "customer service", "hospitality")
